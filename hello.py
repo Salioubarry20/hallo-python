fc723bb9d9594d3ad9ee990eac6b79d221aa2734
unknown <saliou barry>
2021-09-10 12:03:05 +0200

    e

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
-# Saliou 99052276
-print("Hello wereld van")        
+# saliou barry
+print("Hello wereld van")
 print("p   p    y   y       t      h   h   o    o  n   n")
 print("p   p     y y        t      h   h   o    o  n   n")
 print("pppp       y         t      hhhhh   o    o  n   n")
 print("p          y         t      h   h   o    o  n   n")
 print("p          y         t      h   h   o    o  n   n")
 print("p          y         t      h   h    oooo   n   n")
-
